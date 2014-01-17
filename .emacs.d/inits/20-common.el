0db8a32cf6579fb82f7e2133e050a27e468cb4e5
Ryuichi Yamamoto <zryuichi@gmail.com>
2014-01-18 01:09:58 +0900

    [emacs] enable yasnippet [git] delete push setting

--- .emacs.d/inits/20-common.el
+++ .emacs.d/inits/20-common.el
@@ -17,7 +17,7 @@
   (require 'yasnippet)
   (setq yas-snippet-dirs
 	'("~/.emacs.d/snippets"            ;; personal snippets
-	  "~/.emacs.d/yasnippet/snippets"  ;; the default collection
+	  "~/.emacs.d/elpa/yasnippet/snippets"  ;; the default collection
 	  ))
   (yas-global-mode 1)
 )
