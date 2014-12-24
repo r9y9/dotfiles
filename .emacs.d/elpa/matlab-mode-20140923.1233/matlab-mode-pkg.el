963f14a3ae2bf0f41555c33a1f71b93b0e300dd8
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-12-24 15:42:47 +0900

    add new packages

---
+++ .emacs.d/elpa/matlab-mode-20140923.1233/matlab-mode-pkg.el
@@ -0,0 +1,4 @@
+(define-package "matlab-mode" "20140923.1233" "No description available." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/py-autopep8-20140908.31/py-autopep8-autoloads.el
@@ -0,0 +1,27 @@
+;;; py-autopep8-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "py-autopep8" "py-autopep8.el" (21658 22853
+;;;;;;  0 0))
+;;; Generated autoloads from py-autopep8.el
+
+(autoload 'py-autopep8 "py-autopep8" "\
+Formats the current buffer according to the autopep8 tool.
+
+\(fn)" t nil)
+
+(autoload 'py-autopep8-before-save "py-autopep8" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; py-autopep8-autoloads.el ends here
