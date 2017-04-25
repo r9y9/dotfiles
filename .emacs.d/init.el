0c39df4cb1b2b18c0f185b95f9605804d60c101d
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-04-26 01:06:40 +0900

    fix

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,8 +2,6 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
-; (add-to-list 'load-path "~/.emacs.d/manually_installed")
-
 ;; init-loader
 (custom-set-variables
   '(init-loader-show-log-after-init 'error-only))
